963bf9a2eacf163e19f23395c4a4d30a3b7105d7
Kutaiba Bahash <93405663+kutaiba98@users.noreply.github.com>
2023-06-22 17:16:25 +0300

    Add files via upload

--- Gaming/js/main.js
+++ Gaming/js/main.js
@@ -1,6 +1,23 @@
-import { StartPage } from './function.js'
-StartPage();
 
-
-
-
+//writing animation
+function writeText(elem, content) {
+    function animate() {
+      elem.textContent = ""; // ניקוי תוכן האלמנט
+      var contentArray = content.split("");
+      var current = 0;
+      var interval = setInterval(function () {
+        if (current < contentArray.length) {
+          elem.textContent += contentArray[current++];
+        } else {
+          clearInterval(interval);
+          setTimeout(animate, 2000); // קריאה חוזרת לפונקציה לאחר פסקה של 2 שניות
+        }
+      }, 80);
+    }
+  
+    animate();
+  }
+  
+  var holder = document.querySelector("#holder");
+  writeText(holder, "WEB DESIGNER + FRONT-END DEVELOPER");
+  
